0cfe9c8d2e4928fee40918ace789dc226319aab4
Wayne Olson <wayne@wayneo.co>
2022-08-05 02:21:36 +0000

    Fixed typo

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -7,7 +7,7 @@ export const Greeting = () => {
             <h2>Hi, friend...</h2>
             <p className="large">I'm <strong>Wayne</strong>&mdash; a software engineer and dad of two.</p>
             <p>I write about what I'm learning about software development, as well as the journey to making a near mid-life career change into software engineering.</p>
-            <p>Some of the languages and frameworks I have experience with are Javascript (ES6), Node.js, React, jQuery, Ruby, Sinatra, PostgreSQL, and Handlebars.js.</p>
+            <p>Some of the languages and frameworks I have experience with are JavaScript (ES6), Node.js, React, jQuery, Ruby, Sinatra, PostgreSQL, and Handlebars.js.</p>
             <p>I'd love it if you came along for the ride...</p>
             <img
                 src={signature}
